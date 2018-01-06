5d6b58bc41699346222b4558880ad6d7d1c695e0
travelgeezer <travelgeezer@icloud.com>
2018-01-06 16:08:49 +0800

    Added crypto module

---
+++ crypto/src/main/java/com/geezer/crypto/Crypto.kt
@@ -0,0 +1,16 @@
+package com.geezer.crypto
+
+/**
+ * Created by geezer. on 06/01/2018.
+ */
+class Crypto {
+    companion object {
+        fun encrypto(message: String): String {
+            return message
+        }
+
+        fun decrypto(ct: String): String {
+            return ct
+        }
+    }
+}
